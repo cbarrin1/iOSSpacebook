5dfe81a15c94563e8650d95e47752889327b1d55
Charles Barringer <barring_char@bentley.edu>
2019-05-08 20:58:52 -0400

    Initial Commit

---
+++ Spacebook/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Spacebook
+//
+//  Created by Lee Barringer on 5/8/19.
+//  Copyright © 2019 Lee Barringer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: Actions
+    @IBAction func goLogin(_ sender: UIButton) {
+        
+    }
+    
+
+
+}
+
